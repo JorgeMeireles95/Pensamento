1dd9137dc5b8bbc5969cccadf0e6df28f90f90f5
Jorge Meireles <jorgeantoniomeireles@gmail.com>
2023-02-08 19:45:48 -0300

    Creation of the registrapost method and styling of the error message

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,11 @@
+const User = require('../models/User');
+
+
+
+//faz o hash da senha
+const bcrypt = require('bcryptjs');
+
+
 module.exports = class AuthController{
     static login (req, res){
         res.render('auth/login')
@@ -6,6 +14,21 @@ module.exports = class AuthController{
 
     static register(req, res){
         res.render('auth/register')
+    }
+
+
+
+    static async registerPost(req, res){
+        const{name, email, password, confirpassword} = req.body;
+
+       if(password != confirpassword){
+        //
+           req.flash('message', 'As senhas não conferem')
+           res.render('auth/register')
+
+           return 
+       }
+
     }
 }
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,7 @@ const AuthController = require('../controllers/AuthController.js');
 
 router.get('/login', AuthController.login);
 router.get('/register', AuthController.register);
-
+router.post('/register', AuthController.registerPost);
 
 
 
